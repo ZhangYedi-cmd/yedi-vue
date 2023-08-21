53bf5c94a8bdc0b0b7a9ed203f61f61b9ee7b637
yedizhang <zhangyedi@baidu.com>
2023-08-21 20:04:03 +0800

    fix: fix vitest config

--- packages/vue/__tests__/parser.test.ts
+++ packages/vue/__tests__/parser.test.ts
@@ -3,7 +3,13 @@ import {parseHTML} from "../src/parser/parseHtml";
 
 
 describe('html parser test', async() => {
-    test('init', () => {
-        console.log(parseHTML('<div><span>hello yedi</span></div>'));
+    test('test html to ast', () => {
+       let ast = parseHTML('<div><span>hello yedi</span></div>');
+       console.log(ast)
+    })
+
+    test('test ast to virtual dom', () => {
+        let ast = parseHTML('<div><span>hello yedi</span></div>');
+        console.log(ast)
     })
 })
--- packages/vue/vitest.config.ts
+++ packages/vue/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         environment: 'node',
-        root:'./src/__tests__/',
+        root:'./__tests__/',
         passWithNoTests: true,
         exclude: ['**/node_modules/**', '**/dist/**'],
         threads: true
